e16ee14f667c345a26374ac736f0324c8f16776c
Lorenz Gaertner <lorenz.gaertner@gmail.com>
2023-12-25 10:32:26 +0100

    WIP try to schedule

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 # This example requires the 'message_content' intent.
 
 import os
+import datetime
 import discord
+from discord.ext import tasks
 import essen
 
 from dotenv import load_dotenv
@@ -11,9 +13,22 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+foodtime = datetime.time(hour=19, minute=33, second=0) #Create the time on which the task should always run
+
+@tasks.loop(time=foodtime) #Create the task
+async def menu():
+    channel = client.get_channel(1188499642699231254)
+    menu = essen.get_today_menu("mensa-garching")
+    # menu = essen.get_week_menu("mensa-garching", 2023, 40)[0]
+    menu_message = essen.print_menu(menu)
+    await channel.send("Time for lunch!")
+    await channel.send(menu_message)
+
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    if not menu.is_running():
+        menu.start() #If the task is not already running, start it.
+        print("Menu task started")
 
 @client.event
 async def on_message(message):
